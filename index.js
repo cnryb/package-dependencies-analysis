27a811e9cb710a989fa7006c59208d3256b43bc7
任跃兵 <cnryb@live.cn>
2022-09-22 15:59:02 +0800

    tmp

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const { existsSync, readFileSync, writeFileSync } = require('fs')
+const fs = require('fs')
+const path = require('path')
 
-console.log('hello word')
 function main(pathOrPackageName) {
+  const pathDir = path.join(__dirname, pathOrPackageName)
+  const stat = fs.statSync(pathDir)
+  if (stat.isDirectory()) {
+    // 目录
+    const packageJsonContent = fs.readFileSync(path.join(pathDir, 'package.json'))
+    const packageJson = JSON.parse(packageJsonContent)
+    const allDependencies = Object.assign({},packageJson.dependencies,packageJson.devDependencies)
+    console.log(allDependencies)
+  }
 
-  
 }
+
+const pathOrPackageName = process.argv[2]
+console.log(pathOrPackageName)
+main(pathOrPackageName)

b475e2a639a5a45d48bd692dd3841944fa4f386a
任跃兵 <cnryb@live.cn>
2022-09-27 19:01:16 +0800

    效率低

--- index.js
+++ index.js
@@ -5,18 +5,32 @@ const { SemVer, lt, maxSatisfying } = require('semver')
 
 async function main(pathOrPackageName) {
   const pathDir = path.join(__dirname, pathOrPackageName)
-  const stat = fs.statSync(pathDir)
-  let allDependencies = {}
-  if (stat.isDirectory()) {
-    // 目录
-    const packageJsonContent = fs.readFileSync(path.join(pathDir, 'package.json'))
-    const packageJson = JSON.parse(packageJsonContent)
-    allDependencies = Object.assign({}, packageJson.dependencies, packageJson.devDependencies)
-    console.log(allDependencies)
-  } else {
 
+  let allDependencies = {}
+  if (fs.existsSync(pathDir)) {
+    const stat = fs.statSync(pathDir)
+    if (stat.isDirectory()) {
+      // 目录
+      const packageJsonContent = fs.readFileSync(path.join(pathDir, 'package.json'))
+      const packageJson = JSON.parse(packageJsonContent)
+      allDependencies = Object.assign({}, packageJson.dependencies, packageJson.devDependencies)
+    } else {
+      // 文件
+      const packageJsonContent = fs.readFileSync(pathDir)
+      const packageJson = JSON.parse(packageJsonContent)
+      allDependencies = Object.assign({}, packageJson.dependencies, packageJson.devDependencies)
+    }
+
+  } else {
+    const registryInfo = await axios.get(`https://registry.npmjs.org/${pathOrPackageName}`)
+    const chooseVersion = registryInfo.data['dist-tags'].latest
+    const chooseVersionInfo = registryInfo.data.versions[chooseVersion]
+    // allDependencies = Object.assign({}, chooseVersionInfo.dependencies, chooseVersionInfo.devDependencies)
+    allDependencies = Object.assign({}, chooseVersionInfo.dependencies)
   }
+  console.log(allDependencies)
   const packageNames = Object.keys(allDependencies)
+  allDependenciesCount = packageNames.length
   const result = {}
   for (let i = 0; i < packageNames.length; i++) {
     const packageName = packageNames[i]
@@ -28,14 +42,21 @@ async function main(pathOrPackageName) {
   }
   console.log(JSON.stringify(result, null, 2))
 }
+let allDependenciesCount = 0
 
 async function getPackageDependencies(packageName, version) {
-  const registryInfo = await axios.get(`https://registry.npmjs.org/${packageName}`)
-
+  console.log(allDependenciesCount++)
+  // const registryInfo = await axios.get(`https://registry.npmjs.org/${packageName}`)
+  const registryInfo = await axios.get(`https://registry.npmmirror.com/${packageName}`)
+  // https://registry.npmmirror.com/
   const versions = registryInfo.data.versions
-  const versionNums = Object.keys(versions)
-  const chooseMaxVersion = maxSatisfying(versionNums, version)
-  const chooseVersionInfo = versions[chooseMaxVersion]
+  const versionNumbers = Object.keys(versions)
+  let chooseVersion = maxSatisfying(versionNumbers, version)
+  if (!chooseVersion) {
+    chooseVersion = registryInfo.data['dist-tags'][version]
+    if (!chooseVersion) chooseVersion = registryInfo.data['dist-tags'].latest
+  }
+  const chooseVersionInfo = versions[chooseVersion]
   const result = {}
   if (!chooseVersionInfo.dependencies) return result
   const keys = Object.keys(chooseVersionInfo.dependencies)
